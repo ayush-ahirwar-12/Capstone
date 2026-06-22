a7662d3b75845147881b4ee3a871d65d6589636f
ayush ahirwar <ayushahirwar04@gmail.com>
2026-06-22 12:12:44 +0530

    ..

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,20 +1,47 @@
-import express from "express";
-import morgan from "morgan";
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import cookies from 'cookie-parser'
+import passport from 'passport'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+import jwt from 'jsonwebtoken'
 
-const app = express();
+const app = express()
 
-app.use(morgan("dev"));
-app.use(express.json());
+app.use(morgan('dev'))
+app.use(cookies())
+aap.use(passport.initialize())
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  })
+)
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENTID,
+      clientSecret: process.env.CLIENTSECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile)
+    }
+  )
+)
+
+app.set('trust proxy', 1);
+app.get("/_status/healthz", (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
+app.get("/_status/readyz", (req, res) => {
+    res.status(200).json({ status: 'ready' });
+});
+
+app.use('/api/auth', authRoutes);
 
 
-
-
-
-
-
-
-
-
-
-
-export default app;
+export default app
