c78580e6618a27535c6b76a21ed7f74b4a2f94f9
ayush ahirwar <ayushahirwar04@gmail.com>
2026-06-25 00:46:43 +0530

    ..

--- sandbox/router/src/app.js
+++ sandbox/router/src/app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import http from 'http'
 import { createProxyServer } from 'httpxy'
+import { refreshTTL } from './config/redis.js'
 
 const wsproxy = createProxyServer({ changeOrigin: true })
 wsproxy.on('error', (err, req, socket) => {
@@ -47,9 +48,11 @@ function getAgentProxy (sandboxId) {
   return agentProxies[sandboxId]
 }
 
-app.use((req, res, next) => {
+app.use(async(req, res, next) => {
   const host = req.headers.host
   const sandboxId = host.split('.')[0]
+
+  await refreshTTL(sandboxId);
 
   if (host.split('.')[1] === 'agent') {
     return getAgentProxy(sandboxId)(req, res, next)
--- sandbox/router/src/config/redis.js
+++ sandbox/router/src/config/redis.js
@@ -1,15 +1,15 @@
-// import Redis from "ioredis"
+import Redis from "ioredis"
 
-// const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL);
 
-// redis.on('connect', () => {
-//     console.log('Connected to Redis successfully');
-// });
+redis.on('connect', () => {
+    console.log('Connected to Redis successfully');
+});
 
-// redis.on('error', (err) => {
-//     console.error('Redis connection error:', err);
-// });
+redis.on('error', (err) => {
+    console.error('Redis connection error:', err);
+});
 
-// export async function refreshTTL(sandboxId) {
-//     await redis.expire(`sandbox:${sandboxId}`, 120);
-// }
+export async function refreshTTL(sandboxId) {
+    await redis.expire(`sandbox:${sandboxId}`, 120);
+}
